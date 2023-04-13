f5a5736007bf3a01009741632d24bb4999059af2
wahsandaruwan <wahsandaruwan6@gmail.com>
2023-04-13 21:55:21 +0530

    Updated todo dontroller enpoints

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -27,7 +27,7 @@ namespace backend.Controllers
         }
 
         // Get one by id
-        [HttpGet("GetById")]
+        [HttpGet("GetById/{id}")]
         public async Task<ActionResult<Todo>> GetById(int id){
             var todo = await _context.Todos.FindAsync(id);
             if(todo == null){
@@ -47,7 +47,7 @@ namespace backend.Controllers
         }
 
         // Update a todo
-        [HttpPost("Update")]
+        [HttpPut("Update/{id}")]
         public async Task<IActionResult> Update(int id, Todo todo){
             if(id != todo.TodoId){
                 return BadRequest();
