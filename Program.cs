4841574817547ce36a94962d4f03c334e7c72115
wahsandaruwan <wahsandaruwan6@gmail.com>
2023-04-18 06:16:23 +0530

    Added auth parameters

--- Program.cs
+++ Program.cs
@@ -3,12 +3,30 @@ global using backend.Models;
 global using Microsoft.EntityFrameworkCore;
 global using backend.Database;
 
+// Local directives
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // For CORS
 builder.Services.AddCors(p => p.AddPolicy("corspolicy", build => build.WithOrigins("http://127.0.0.1:5173").AllowAnyMethod().AllowAnyHeader()));
 builder.Services.AddControllers();
+
+// For auth
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,6 +47,9 @@ if (app.Environment.IsDevelopment())
 app.UseCors("corspolicy");
 
 app.UseHttpsRedirection();
+
+// For auth
+app.UseAuthentication();
 
 app.UseAuthorization();
 
